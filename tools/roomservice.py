AOSPA/android_build

build: Roomservice for Paranoid Android
- Remove the device search/sync
- Use vendor/pa to host dependencies files
- Remvoe GitHub dependency
- Clean up comments and code
- Refactor error/warning messages
- Use --force-sync instead of -f

Change-Id: Ic68c91d467c23d0f47fac4d8a6e8d56f6ddb0993

tools/roomservice.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 
+# Copyright (C) 2015 The Paranoid Android Project
 # Copyright (C) 2013 Cybojenix <anthonydking@gmail.com>
 # Copyright (C) 2013 The OmniROM Project
 #
@@ -33,73 +34,17 @@ except ImportError:
     urllib = imp.new_module('urllib')
     urllib.request = urllib2
 
-# Config
-# set this to the default remote to use in repo
-default_rem = "github"
-# set this to the default revision to use (branch/tag name)
+# Default remote to use in repo
+default_rem = "aospa"
+# Default revision to use (branch/tag name)
 default_rev = "marshmallow"
-# set this to the remote that you use for projects from your team repos
-# example fetch="https://github.com/omnirom"
-default_team_rem = "github"
-# this shouldn't change unless google makes changes
+
+# Default local manifest location
 local_manifest_dir = ".repo/local_manifests"
-# change this to your name on github (or equivalent hosting)
-android_team = "AOSPA"
+# Default dependencies location
+dependencies_dir = "vendor/pa/products"
 
-
-def check_repo_exists(git_data):
-    if not int(git_data.get('total_count', 0)):
-        raise Exception("{} not found in {} Github, exiting "
-                        "roomservice".format(device, android_team))
-
-
-# Note that this can only be done 5 times per minute
-def search_github_for_device(device):
-    git_device = '+'.join(re.findall('[a-z]+|[\d]+',  device))
-    git_search_url = "https://api.github.com/search/repositories" \
-                     "?q=%40{}+android_device+{}+fork:true".format(android_team, git_device)
-    git_req = urllib.request.Request(git_search_url)
-    try:
-        response = urllib.request.urlopen(git_req)
-    except urllib.request.HTTPError:
-        raise Exception("There was an issue connecting to github."
-                        " Please try again in a minute")
-    git_data = json.load(response)
-    check_repo_exists(git_data)
-    print("found the {} device repo".format(device))
-    return git_data
-
-
-def get_device_url(git_data):
-    device_url = ""
-    for item in git_data['items']:
-        temp_url = item.get('html_url')
-        if "{}/android_device".format(android_team) in temp_url:
-            try:
-                temp_url = temp_url[temp_url.index("android_device"):]
-            except ValueError:
-                pass
-            else:
-                if temp_url.endswith(device):
-                    device_url = temp_url
-                    break
-
-    if device_url:
-        return device_url
-    raise Exception("{} not found in {} Github, exiting "
-                    "roomservice".format(device, android_team))
-
-
-def parse_device_directory(device_url,device):
-    to_strip = "android_device"
-    repo_name = device_url[device_url.index(to_strip) + len(to_strip):]
-    repo_name = repo_name[:repo_name.index(device)]
-    repo_dir = repo_name.replace("_", "/")
-    repo_dir = repo_dir + device
-    return "device{}".format(repo_dir)
-
-
-# Thank you RaYmAn
+# Contribution by RaYmAn
 def iterate_manifests(check_all):
     files = []
     if check_all:
@@ -111,7 +56,7 @@ def iterate_manifests(check_all):
             man = ES.parse(file)
             man = man.getroot()
         except IOError, ES.ParseError:
-            print("WARNING: error while parsing %s" % file)
+            print("WARNING: Error while parsing %s" % file)
         else:
             for project in man.findall("project"):
                 yield project
@@ -197,7 +142,7 @@ def write_to_manifest(manifest):
 
     with open('/'.join([local_manifest_dir, "roomservice.xml"]), 'w') as f:
         f.write(raw_xml)
-    print("wrote the new roomservice manifest")
+    print("Wrote the new roomservice manifest")
 
 
 def parse_device_from_manifest(device):
@@ -225,9 +170,8 @@ def parse_device_from_folder(device):
     return location
 
 
-def parse_dependency_file(location):
-    dep_file = "pa.dependencies"
-    dep_location = '/'.join([location, dep_file])
+def parse_dependency_file(device):
+    dep_location = dependencies_dir + '/{}/pa.dependencies'.format(device)
     if not os.path.isfile(dep_location):
         print("WARNING: %s file not found" % dep_location)
         sys.exit()
@@ -235,7 +179,7 @@ def parse_dependency_file(location):
         with open(dep_location, 'r') as f:
             dependencies = json.loads(f.read())
     except ValueError:
-        raise Exception("ERROR: malformed dependency file")
+        raise Exception("ERROR: Malformed dependency file")
     return dependencies
 
 
@@ -247,11 +191,6 @@ def create_dependency_manifest(dependencies):
         revision = dependency.get("revision", default_rev)
         remote = dependency.get("remote", default_rem)
 
-        # not adding an organization should default to android_team
-        # only apply this to github
-        if remote == "github":
-            if not "/" in repository:
-                repository = '/'.join([android_team, repository])
         project = create_manifest_project(repository,
                                           target_path,
                                           remote=remote,
@@ -261,15 +200,11 @@ def create_dependency_manifest(dependencies):
             write_to_manifest(manifest)
             projects.append(target_path)
     if len(projects) > 0:
-        os.system("repo sync -f --no-clone-bundle %s" % " ".join(projects))
+        os.system("repo sync --force-sync --no-clone-bundle %s" % " ".join(projects))
 
 
 def fetch_dependencies(device):
-    location = parse_device_from_folder(device)
-    if location is None or not os.path.isdir(location):
-        raise Exception("ERROR: could not find your device "
-                        "folder location, bailing out")
-    dependencies = parse_dependency_file(location)
+    dependencies = parse_dependency_file(device)
     create_dependency_manifest(dependencies)
 
 
@@ -278,23 +213,6 @@ def check_device_exists(device):
     if location is None:
         return False
     return os.path.isdir(location)
-
-
-def fetch_device(device):
-    if check_device_exists(device):
-        print("WARNING: Trying to fetch a device that's already there")
-        return
-    git_data = search_github_for_device(device)
-    device_url = android_team+"/"+get_device_url(git_data)
-    device_dir = parse_device_directory(device_url,device)
-    project = create_manifest_project(device_url,
-                                      device_dir,
-                                      remote=default_team_rem)
-    if not project is None:
-        manifest = append_to_manifest(project)
-        write_to_manifest(manifest)
-        print("syncing the device config")
-        os.system('repo sync -f --no-clone-bundle %s' % device_dir)
 
 
 if __name__ == '__main__':
@@ -307,11 +225,4 @@ if __name__ == '__main__':
     except ValueError:
         device = product
 
-    if len(sys.argv) > 2:
-        deps_only = sys.argv[2]
-    else:
-        deps_only = False
-
-    if not deps_only:
-        fetch_device(device)
     fetch_dependencies(device)

BEAOSP/build

tools/roomservice.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 
+# Copyright (C) 2015 The Paranoid Android Project
 # Copyright (C) 2013 Cybojenix <anthonydking@gmail.com>
 # Copyright (C) 2013 The OmniROM Project
 #
@@ -33,73 +34,16 @@ except ImportError:
     urllib = imp.new_module('urllib')
     urllib.request = urllib2
 
-# Config
-# set this to the default remote to use in repo
-default_rem = "github"
-# set this to the default revision to use (branch/tag name)
-default_rev = "marshmallow"
-# set this to the remote that you use for projects from your team repos
-# example fetch="https://github.com/omnirom"
-default_team_rem = "github"
-# this shouldn't change unless google makes changes
+# Default remote to use in repo
+default_rem = "bbq"
+# Default revision to use (branch/tag name)
+default_rev = "oreo"
+# Default local manifest location
 local_manifest_dir = ".repo/local_manifests"
-# change this to your name on github (or equivalent hosting)
-android_team = "BBQDroid"
+# Default dependencies location
+dependencies_dir = "vendor/bbq/products"
 
-
-def check_repo_exists(git_data):
-    if not int(git_data.get('total_count', 0)):
-        raise Exception("{} not found in {} Github, exiting "
-                        "roomservice".format(device, android_team))
-
-
-# Note that this can only be done 5 times per minute
-def search_github_for_device(device):
-    git_device = '+'.join(re.findall('[a-z]+|[\d]+',  device))
-    git_search_url = "https://api.github.com/search/repositories" \
-                     "?q=%40{}+android_device+{}+fork:true".format(android_team, git_device)
-    git_req = urllib.request.Request(git_search_url)
-    try:
-        response = urllib.request.urlopen(git_req)
-    except urllib.request.HTTPError:
-        raise Exception("There was an issue connecting to github."
-                        " Please try again in a minute")
-    git_data = json.load(response)
-    check_repo_exists(git_data)
-    print("found the {} device repo".format(device))
-    return git_data
-
-
-def get_device_url(git_data):
-    device_url = ""
-    for item in git_data['items']:
-        temp_url = item.get('html_url')
-        if "{}/android_device".format(android_team) in temp_url:
-            try:
-                temp_url = temp_url[temp_url.index("android_device"):]
-            except ValueError:
-                pass
-            else:
-                if temp_url.endswith(device):
-                    device_url = temp_url
-                    break
-
-    if device_url:
-        return device_url
-    raise Exception("{} not found in {} Github, exiting "
-                    "roomservice".format(device, android_team))
-
-
-def parse_device_directory(device_url,device):
-    to_strip = "android_device"
-    repo_name = device_url[device_url.index(to_strip) + len(to_strip):]
-    repo_name = repo_name[:repo_name.index(device)]
-    repo_dir = repo_name.replace("_", "/")
-    repo_dir = repo_dir + device
-    return "device{}".format(repo_dir)
-
-
-# Thank you RaYmAn
+# Contribution by RaYmAn
 def iterate_manifests(check_all):
     files = []
     if check_all:
@@ -111,7 +55,7 @@ def iterate_manifests(check_all):
             man = ES.parse(file)
             man = man.getroot()
         except IOError, ES.ParseError:
-            print("WARNING: error while parsing %s" % file)
+            print("WARNING: Error while parsing %s" % file)
         else:
             for project in man.findall("project"):
                 yield project
@@ -197,7 +141,7 @@ def write_to_manifest(manifest):
 
     with open('/'.join([local_manifest_dir, "roomservice.xml"]), 'w') as f:
         f.write(raw_xml)
-    print("wrote the new roomservice manifest")
+    print("Wrote the new roomservice manifest")
 
 
 def parse_device_from_manifest(device):
@@ -225,9 +169,8 @@ def parse_device_from_folder(device):
     return location
 
 
-def parse_dependency_file(location):
-    dep_file = "pa.dependencies"
-    dep_location = '/'.join([location, dep_file])
+def parse_dependency_file(device):
+    dep_location = dependencies_dir + '/{}/pa.dependencies'.format(device)
     if not os.path.isfile(dep_location):
         print("WARNING: %s file not found" % dep_location)
         sys.exit()
@@ -235,7 +178,7 @@ def parse_dependency_file(location):
         with open(dep_location, 'r') as f:
             dependencies = json.loads(f.read())
     except ValueError:
-        raise Exception("ERROR: malformed dependency file")
+        raise Exception("ERROR: Malformed dependency file")
     return dependencies
 
 
@@ -247,11 +190,6 @@ def create_dependency_manifest(dependencies):
         revision = dependency.get("revision", default_rev)
         remote = dependency.get("remote", default_rem)
 
-        # not adding an organization should default to android_team
-        # only apply this to github
-        if remote == "github":
-            if not "/" in repository:
-                repository = '/'.join([android_team, repository])
         project = create_manifest_project(repository,
                                           target_path,
                                           remote=remote,
@@ -261,15 +199,11 @@ def create_dependency_manifest(dependencies):
             write_to_manifest(manifest)
             projects.append(target_path)
     if len(projects) > 0:
-        os.system("repo sync -f --no-clone-bundle %s" % " ".join(projects))
+        os.system("repo sync --force-sync --no-clone-bundle %s" % " ".join(projects))
 
 
 def fetch_dependencies(device):
-    location = parse_device_from_folder(device)
-    if location is None or not os.path.isdir(location):
-        raise Exception("ERROR: could not find your device "
-                        "folder location, bailing out")
-    dependencies = parse_dependency_file(location)
+    dependencies = parse_dependency_file(device)
     create_dependency_manifest(dependencies)
 
 
@@ -278,23 +212,6 @@ def check_device_exists(device):
     if location is None:
         return False
     return os.path.isdir(location)
-
-
-def fetch_device(device):
-    if check_device_exists(device):
-        print("WARNING: Trying to fetch a device that's already there")
-        return
-    git_data = search_github_for_device(device)
-    device_url = android_team+"/"+get_device_url(git_data)
-    device_dir = parse_device_directory(device_url,device)
-    project = create_manifest_project(device_url,
-                                      device_dir,
-                                      remote=default_team_rem)
-    if not project is None:
-        manifest = append_to_manifest(project)
-        write_to_manifest(manifest)
-        print("syncing the device config")
-        os.system('repo sync -f --no-clone-bundle %s' % device_dir)
 
 
 if __name__ == '__main__':
@@ -307,11 +224,4 @@ if __name__ == '__main__':
     except ValueError:
         device = product
 
-    if len(sys.argv) > 2:
-        deps_only = sys.argv[2]
-    else:
-        deps_only = False
-
-    if not deps_only:
-        fetch_device(device)
     fetch_dependencies(device)